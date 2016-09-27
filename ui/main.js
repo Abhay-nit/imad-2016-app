9841cb0e701969180c595c82a84cf84a6a0538a6
Abhay-nit <abhaysk11@gmail.com>
2016-09-27 12:37:18 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,3 +1,8 @@
 console.log('Loaded!');
 var element=document.getElementById("abhay");
 element.innerHTML="new value changed by me";
+var image=document.getElementById("madi");
+image.onClick=function()
+{
+    image.style.marginleft="200px";
+};
